313fb087b7d589f2511345830dcc8c60fe7120a1
云却 <479813005@qq.com>
2023-04-07 15:25:14 +0800

    拆分特性

---
+++ Runtime/Attribute/ReadOnlyInInspectorAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Megumin;
+using UnityEngine;
+
+namespace Megumin
+{
+    /// <summary>
+    /// 让字段在编辑器中只读，防止被错误的修改
+    /// </summary>
+    [System.AttributeUsage(System.AttributeTargets.Field)]
+    public sealed class ReadOnlyInInspectorAttribute : PropertyAttribute
+    {
+
+    }
+
+#if UNITY_EDITOR
+
+#if !DISABLE_MEGUMIN_PROPERTYDRWAER
+    [UnityEditor.CustomPropertyDrawer(typeof(ReadOnlyInInspectorAttribute))]
+#endif
+    public class LockedInInspectorAttributeDrawer : UnityEditor.PropertyDrawer
+    {
+        public override float GetPropertyHeight(UnityEditor.SerializedProperty property, GUIContent label)
+        {
+            return UnityEditor.EditorGUI.GetPropertyHeight(property);
+        }
+
+        public override void OnGUI(Rect position, UnityEditor.SerializedProperty property, GUIContent label)
+        {
+            using (new UnityEditor.EditorGUI.DisabledGroupScope(true))
+            {
+                UnityEditor.EditorGUI.PropertyField(position, property, label, true);
+            }
+        }
+    }
+
+#endif
+}
