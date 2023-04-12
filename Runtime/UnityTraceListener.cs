baf5418f419e631abc7ae0b5eca7fb5f074c5a58
云却 <479813005@qq.com>
2023-04-12 19:46:12 +0800

    整理log控制台

--- Runtime/UnityTraceListener.cs
+++ Runtime/UnityTraceListener.cs
@@ -1,16 +1,19 @@
-using System;
+﻿using System;
 using System.Diagnostics;
+using UnityEngine;
 using Debug = UnityEngine.Debug;
 
 namespace Megumin
 {
     public class UnityTraceListener : TraceListener
     {
+        [HideInCallstack]
         public override void Write(string message)
         {
             Debug.Log(message);
         }
 
+        [HideInCallstack]
         public override void Write(object o, string category)
         {
             if (category.Contains("Warning", StringComparison.OrdinalIgnoreCase))
@@ -23,6 +26,7 @@ namespace Megumin
             }
         }
 
+        [HideInCallstack]
         public override void Write(string message, string category)
         {
             if (category.Contains("Warning", StringComparison.OrdinalIgnoreCase))
@@ -35,11 +39,13 @@ namespace Megumin
             }
         }
 
+        [HideInCallstack]
         public override void WriteLine(string message)
         {
             Debug.Log(message);
         }
 
+        [HideInCallstack]
         public override void WriteLine(object o, string category)
         {
             if (category.Contains("Warning", StringComparison.OrdinalIgnoreCase))
@@ -52,6 +58,7 @@ namespace Megumin
             }
         }
 
+        [HideInCallstack]
         public override void WriteLine(string message, string category)
         {
             if (category.Contains("Warning", StringComparison.OrdinalIgnoreCase))
@@ -64,14 +71,37 @@ namespace Megumin
             }
         }
 
+        [HideInCallstack]
         public override void Fail(string message, string detailMessage)
         {
             Debug.LogError($"{message}\n{detailMessage}");
         }
 
+        [HideInCallstack]
         public override void Fail(string message)
         {
             Debug.LogError(message);
         }
     }
 }
+
+#if UNITY_2022_2_OR_NEWER
+    //Unity 内置
+#else
+
+namespace UnityEngine
+{
+    //
+    // 摘要:
+    //     Marks the methods you want to hide from the Console window callstack. When you
+    //     hide these methods they are removed from the detail area of the selected message
+    //     in the Console window.
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class HideInCallstackAttribute : Attribute
+    {
+    }
+}
+
+#endif
+
+
