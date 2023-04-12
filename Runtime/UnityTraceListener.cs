d56824875eb74617fc11388a172d95cb45a728de
云却 <479813005@qq.com>
2023-04-12 19:40:14 +0800

    增量loglistener

---
+++ Runtime/UnityTraceListener.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+namespace Megumin
+{
+    public class UnityTraceListener : TraceListener
+    {
+        public override void Write(string message)
+        {
+            Debug.Log(message);
+        }
+
+        public override void Write(object o, string category)
+        {
+            if (category.Contains("Warning", StringComparison.OrdinalIgnoreCase))
+            {
+                Debug.LogWarning(o);
+            }
+            else
+            {
+                base.Write(o, category);
+            }
+        }
+
+        public override void Write(string message, string category)
+        {
+            if (category.Contains("Warning", StringComparison.OrdinalIgnoreCase))
+            {
+                Debug.LogWarning(message);
+            }
+            else
+            {
+                base.Write(message, category);
+            }
+        }
+
+        public override void WriteLine(string message)
+        {
+            Debug.Log(message);
+        }
+
+        public override void WriteLine(object o, string category)
+        {
+            if (category.Contains("Warning", StringComparison.OrdinalIgnoreCase))
+            {
+                Debug.LogWarning(o);
+            }
+            else
+            {
+                base.WriteLine(o, category);
+            }
+        }
+
+        public override void WriteLine(string message, string category)
+        {
+            if (category.Contains("Warning", StringComparison.OrdinalIgnoreCase))
+            {
+                Debug.LogWarning(message);
+            }
+            else
+            {
+                base.WriteLine(message, category);
+            }
+        }
+
+        public override void Fail(string message, string detailMessage)
+        {
+            Debug.LogError($"{message}\n{detailMessage}");
+        }
+
+        public override void Fail(string message)
+        {
+            Debug.LogError(message);
+        }
+    }
+}
