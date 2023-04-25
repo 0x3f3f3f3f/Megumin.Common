e2f2bd6254161e24a567aac28a8abcdcdb816ec2
云却 <479813005@qq.com>
2023-04-25 20:07:03 +0800

    增加 TypeSetterAttribute特性

--- Runtime/Attribute/TypeSetterAttribute.cs
+++ Runtime/Attribute/TypeSetterAttribute.cs
@@ -15,6 +15,7 @@ namespace Megumin
 
 namespace UnityEditor.Megumin
 {
+    using System.Collections.Generic;
     using UnityEditor;
 
 #if !DISABLE_MEGUMIN_PROPERTYDRWAER
@@ -64,6 +65,18 @@ namespace UnityEditor.Megumin
         public GenericMenu Menu { get; private set; }
         public TaskCompletionSource<Type> Source { get; private set; }
 
+        class TypeItem : IComparable<TypeItem>
+        {
+            public Type Type { get; internal set; }
+            public GUIContent GUIContent { get; internal set; }
+            public string FirstC { get; internal set; }
+
+            public int CompareTo(TypeItem other)
+            {
+                return GUIContent.text.CompareTo(other.GUIContent.text);
+            }
+        }
+
         public override Task<Type> GetResult(object options = null)
         {
             Source = new();
@@ -71,14 +84,63 @@ namespace UnityEditor.Megumin
             if (Menu == null)
             {
                 Menu = new GenericMenu();
-
                 var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).ToList();
-                foreach (var type in types)
+                List<TypeItem> typeItems = new();
+                System.Random random = new();
+
+                Task GTypeItem(int start, int count)
                 {
-                    Menu.AddItem(new GUIContent(type.FullName), false, Callback, type);
+                    return Task.Run(() =>
+                    {
+                        for (int i = start; i < start + count; i++)
+                        {
+                            if (i < types.Count)
+                            {
+                                var type = types[i];
+                                TypeItem typeItem = new();
+                                typeItem.Type = type;
+                                typeItem.FirstC = type.Name[0].ToString().ToUpper();
+                                //typeItem.GUIContent = new($"{typeItem.FirstC}/{type.Namespace}/{type.Name}");
+                                typeItem.GUIContent = new($"{typeItem.FirstC}/{type.FullName?.Replace('.', '/')}");
+                                typeItems.Add(typeItem);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    });
+                }
+
+                List<Task> tasks = new();
+
+                const int batchCount = 2000;
+                var batch = types.Count / batchCount + 1;
+                for (int i = 0; i < batch; i++)
+                {
+                    tasks.Add(GTypeItem(i * batchCount, batchCount));
+                }
+
+                while (tasks.Any(elem => elem.IsCompleted == false))
+                {
+                    EditorUtility.DisplayProgressBar("CacheMenuItem", "Wait....", (float)typeItems.Count / types.Count);
+                }
+
+                //Task.WaitAll(tasks.ToArray());
+
+                typeItems.Sort();
+                for (int i = 0; i < typeItems.Count; i++)
+                {
+                    TypeItem item = typeItems[i];
+                    if (item != null)
+                    {
+                        EditorUtility.DisplayProgressBar("CacheMenuItem", "AddItem....", (float)i / typeItems.Count);
+                        Menu.AddItem(item.GUIContent, false, Callback, item.Type);
+                    }
                 }
             }
 
+            EditorUtility.ClearProgressBar();
             Menu.ShowAsContext();
             return Source.Task;
         }
