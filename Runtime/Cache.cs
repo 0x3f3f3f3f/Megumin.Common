5da65d58de9a40aaa0fc5b802385aebadab3dfc6
云却 <479813005@qq.com>
2023-09-03 20:18:58 +0800

    增加一种常用的多层缓存机制

---
+++ Runtime/Cache.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Megumin
+{
+    /// <summary>
+    /// 一种常用的多层缓存机制
+    /// </summary>
+    /// <typeparam name="K"></typeparam>
+    /// <typeparam name="V"></typeparam>
+    /// <remarks>
+    /// Q: 在传递K key参数时，要不要使用in 参数修饰符？
+    /// <para/> https://learn.microsoft.com/zh-cn/dotnet/csharp/language-reference/keywords/in-parameter-modifier
+    /// A: 决定尽可能的使用in。
+    /// 只有在K 为int 或者更小的类型时，in会导致更多的开销。
+    /// Cache通常用于性能敏感业务，调用次数会非常高，大结构类型做K时，有必要使用in优化。
+    /// </remarks>
+    public abstract class Cache<K, V>
+    {
+        /// <summary>
+        /// 根据Key查找结果
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="forceReCache">是否强制重新计算并更新缓存</param>
+        /// <param name="option">备用参数，可能的包含缓存淘汰机制相关参数</param>
+        /// <returns></returns>
+        public ValueTask<V> Get(in K key, bool forceReCache = false, object option = null)
+        {
+            if (forceReCache == false && TryGetCache(in key, out var result, option))
+            {
+                return new ValueTask<V>(result: result);
+            }
+            else
+            {
+                return ReCache(key, forceReCache, option);
+            }
+        }
+
+        /// <summary>
+        /// 重新计算结果，并更新到缓存中
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="forceReCache"></param>
+        /// <param name="option"></param>
+        /// <returns></returns>
+        public virtual async ValueTask<V> ReCache(K key, bool forceReCache = false, object option = null)
+        {
+            var result = await Calculate(in key, forceReCache, option);
+            UpdateCache(in key, result, forceReCache, option);
+            return result;
+        }
+
+        /// <summary>
+        /// 从缓存中获取结果
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="option">备用参数，可能的包含缓存淘汰机制相关参数</param>
+        /// <returns></returns>
+        /// <remarks>
+        /// 缓存过期机制在这个方法内部实现
+        /// </remarks>
+        public abstract bool TryGetCache(in K key, out V value, object option = null);
+
+        /// <summary>
+        /// 计算结果
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="forceReCache"></param>
+        /// <param name="option"></param>
+        /// <returns></returns>
+        public abstract ValueTask<V> Calculate(in K key, bool forceReCache, object option = null);
+
+        /// <summary>
+        /// 更新结果到缓存中
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="forceReCache"></param>
+        /// <param name="option"></param>
+        public abstract void UpdateCache(in K key, V value, bool forceReCache, object option = null);
+        public abstract bool ClearCache();
+    }
+
+    public abstract class DictionaryCache<K, V> : Cache<K, V>
+    {
+        protected static Dictionary<K, V> CacheDic { get; } = new();
+
+        public override bool TryGetCache(in K key, out V value, object option = null)
+        {
+            return CacheDic.TryGetValue(key, out value);
+        }
+
+        public override void UpdateCache(in K key, V value, bool forceReCache, object option = null)
+        {
+            CacheDic[key] = value;
+        }
+
+        public override bool ClearCache()
+        {
+            CacheDic.Clear();
+            return true;
+        }
+    }
+}
+
+
