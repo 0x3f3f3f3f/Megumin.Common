c3c773971e1c01f16e1c75c3d787da937ec5b400
云却 <479813005@qq.com>
2023-10-29 10:17:28 +0800

    增加扩展函数

---
+++ Runtime/UnityExtension.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine.Networking;
+using System.IO;
+using Megumin;
+using System.Runtime.CompilerServices;
+using UnityEngine.Profiling;
+using System.Runtime.InteropServices;
+using System.Linq;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Megumin
+{
+    /// <summary>
+    /// 常用扩展
+    /// </summary>
+    public static class UnityExtension_765D8FDEA6B04280BDF031886E30566C
+    {
+        static MethodInfo OpenPropertyEditorMethod;
+
+        /// <summary>
+        /// 通过反射打开属性面板
+        /// </summary>
+        /// <param name="object"></param>
+        [System.Diagnostics.Conditional("UNITY_EDITOR")]
+        public static void OpenPropertyEditor(this UnityEngine.Object @object)
+        {
+#if UNITY_EDITOR
+
+            if (OpenPropertyEditorMethod == null)
+            {
+                var ab = Assembly.GetAssembly(typeof(EditorWindow));
+                var propertyEditor = ab.GetType("UnityEditor.PropertyEditor");
+                OpenPropertyEditorMethod = propertyEditor.GetMethod("OpenPropertyEditor",
+                                                              BindingFlags.NonPublic
+                                                              | BindingFlags.Static);
+            }
+
+            OpenPropertyEditorMethod?.Invoke(null, new object[] { @object, true });
+#endif
+        }
+    }
+}
+
+
+
