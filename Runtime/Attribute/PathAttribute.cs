61c7c04d32a395cf5917c4830e00c7b558064b63
云却 <479813005@qq.com>
2023-12-31 13:17:03 +0800

    按钮和属性间隔6改为4

--- Runtime/Attribute/PathAttribute.cs
+++ Runtime/Attribute/PathAttribute.cs
@@ -31,7 +31,7 @@ namespace UnityEditor.Megumin
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             var propertyPosition = position;
-            propertyPosition.width -= 86;
+            propertyPosition.width -= 84;
 
             var buttonPosition = position;
             buttonPosition.width = 80;
