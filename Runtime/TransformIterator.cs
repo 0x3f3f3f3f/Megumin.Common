be981acf0f85c55878c6ae4a0bf896145e11d1b7
云却 <479813005@qq.com>
2024-01-30 11:35:35 +0800

    增加编辑器测试菜单。重构重命名

--- Runtime/TransformIterator.cs
+++ Runtime/TransformIterator.cs
@@ -6,6 +6,9 @@ using Pool = UnityEngine.Pool.ListPool<UnityEngine.Transform>;
 
 namespace Megumin
 {
+    //本来想加入过滤未开启节点的设置的。但是会导致复杂性增加。
+    //用户根据遍历结果自行过滤更好。
+
     /// <summary>
     /// Transform Iterator，广度优先（breadth first search BFS）。无GC Alloc。
     /// </summary>
@@ -19,21 +22,21 @@ namespace Megumin
         public bool IncludeRoot;
 
         /// <summary>
-        /// 无论是否包含root自身，root 深度为第0层。默认值为2，遍历root和他的子元素
+        /// 无论是否包含root自身，root 深度为第0层。默认值为3，遍历root，子节点，和子节点的子节点。如果层数小于3，使用普通遍历更方便，没必要使用此类。
         /// </summary>
-        public int DeepCount;
+        public int MaxDepth;
 
         /// <summary>
         /// Transform Iterator，广度优先（breadth first search BFS）。无GC Alloc。
         /// </summary>
         /// <param name="root"></param>
-        /// <param name="deepCount">无论是否包含root自身，root 深度为第0层。默认值为2，遍历root和他的子元素</param>
+        /// <param name="maxDepth">无论是否包含root自身，root 深度为第0层。默认值为3，遍历root，子节点，和子节点的子节点。如果层数小于3，使用普通遍历更方便，没必要使用此类。</param>
         /// <param name="includeRoot">遍历结果收包含root本身。默认包括</param>
-        public TransformBFS(Transform root, int deepCount = 2, bool includeRoot = true)
+        public TransformBFS(Transform root, int maxDepth = 3, bool includeRoot = true)
         {
             Root = root;
             IncludeRoot = includeRoot;
-            DeepCount = deepCount;
+            MaxDepth = maxDepth;
         }
 
         public Enumerator GetEnumerator()
@@ -53,7 +56,7 @@ namespace Megumin
                 this.Interater = interater;
 
                 index = -1;
-                deepIndex = 0;
+                depthIndex = 0;
                 currentLayer = Pool.Get();
                 nextLayer = Pool.Get();
 
@@ -66,7 +69,7 @@ namespace Megumin
             /// <summary>
             /// 层号
             /// </summary>
-            public int deepIndex;
+            public int depthIndex;
             //Queue的实现思路，没办法处理层号。而且Queue没有内置池。
             List<Transform> currentLayer;
             List<Transform> nextLayer;
@@ -78,8 +81,8 @@ namespace Megumin
                 if (index >= currentLayer.Count)
                 {
                     //当前层没有元素。切换到下一层。
-                    deepIndex++;
-                    if (deepIndex >= Interater.DeepCount)
+                    depthIndex++;
+                    if (depthIndex >= Interater.MaxDepth)
                     {
                         return false;
                     }
@@ -97,12 +100,12 @@ namespace Megumin
                     nextLayer.Clear();
                 }
 
-                if (deepIndex < Interater.DeepCount)
+                if (depthIndex < Interater.MaxDepth)
                 {
                     //当前层；
                     var current = currentLayer[index];
 
-                    //将当前对象的子对象加入下一层
+                    //将当前节点的子节点加入下一层
                     for (int i = 0; i < current.childCount; i++)
                     {
                         nextLayer.Add(current.GetChild(i));
@@ -128,7 +131,7 @@ namespace Megumin
                     //这里会跳过根节点的MoveNext,手动将子节点加入下一层
                     var current = Interater.Root;
 
-                    //将当前对象的子对象加入下一层
+                    //将当前节点的子节点加入下一层
                     for (int i = 0; i < current.childCount; i++)
                     {
                         nextLayer.Add(current.GetChild(i));
@@ -144,6 +147,35 @@ namespace Megumin
                 Pool.Release(nextLayer);
             }
         }
+
+#if UNITY_EDITOR
+
+        [UnityEditor.MenuItem("GameObject/Megumin/Transform BFS Depth3", priority = 30)]
+        static void BFSDepth3(UnityEditor.MenuCommand menuCommand)
+        {
+            if (menuCommand.context is GameObject go)
+            {
+                foreach (var item in new TransformBFS(go.transform))
+                {
+                    Debug.Log(item.name);
+                }
+            }
+        }
+
+        [UnityEditor.MenuItem("GameObject/Megumin/Transform BFS Depth All", priority = 31)]
+        static void BFSDepthAll(UnityEditor.MenuCommand menuCommand)
+        {
+            if (menuCommand.context is GameObject go)
+            {
+                foreach (var item in new TransformBFS(go.transform, 99))
+                {
+                    Debug.Log(item.name);
+                }
+            }
+        }
+
+#endif
+
     }
 
     /// <summary>
@@ -158,21 +190,21 @@ namespace Megumin
         public bool IncludeRoot;
 
         /// <summary>
-        /// 无论是否包含root自身，root 深度为第0层。默认值为2，遍历root和他的子元素
+        /// 无论是否包含root自身，root 深度为第0层。默认值为3，遍历root，子节点，和子节点的子节点。如果层数小于3，使用普通遍历更方便，没必要使用此类。
         /// </summary>
-        public int DeepCount;
+        public int MaxDepth;
 
         /// <summary>
         /// Transform Iterator，深度优先（depth first search DFS）。无GC Alloc。
         /// </summary>
         /// <param name="root"></param>
-        /// <param name="deepCount">无论是否包含root自身，root 深度为第0层。默认值为2，遍历root和他的子元素</param>
+        /// <param name="maxDepth">无论是否包含root自身，root 深度为第0层。默认值为3，遍历root，子节点，和子节点的子节点。如果层数小于3，使用普通遍历更方便，没必要使用此类。</param>
         /// <param name="includeRoot">遍历结果收包含root本身。默认包括</param>
-        public TransformDFS(Transform root, int deepCount = 2, bool includeRoot = true)
+        public TransformDFS(Transform root, int maxDepth = 3, bool includeRoot = true)
         {
             Root = root;
             IncludeRoot = includeRoot;
-            DeepCount = deepCount;
+            MaxDepth = maxDepth;
         }
 
         public Enumerator GetEnumerator()
@@ -190,7 +222,7 @@ namespace Megumin
             {
                 this.Interater = interater;
                 index = -1;
-                deepIndex = 0;
+                depthIndex = 0;
                 if (interater.IncludeRoot)
                 {
                     currentRoot = null;
@@ -202,14 +234,14 @@ namespace Megumin
             }
 
             public int index;
-            public int deepIndex;
+            public int depthIndex;
             public Transform Current
             {
                 get
                 {
                     if (index == -1)
                     {
-                        //-1返回节点本身，0返回第一个子元素。
+                        //-1返回节点本身，0返回第一个子节点。
                         return currentRoot;
                     }
                     return currentRoot.GetChild(index);
@@ -223,14 +255,14 @@ namespace Megumin
                 {
                     currentRoot = Interater.Root;
                     index = -1;
-                    deepIndex = 0;
+                    depthIndex = 0;
                     return true;
                 }
 
                 //更改根节点
 
                 //尝试向下一层搜索
-                if (deepIndex < Interater.DeepCount - 2)
+                if (depthIndex < Interater.MaxDepth - 2)
                 {
                     if (index >= 0)//表示节点本身已经是子节点了。当index == -1时，表示节点还没有执行子节点本身，不要向下更换根节点。
                     {
@@ -238,8 +270,8 @@ namespace Megumin
                         if (current.childCount > 0)
                         {
                             //深入一层
-                            deepIndex++;
-                            //父对象设置为当前对象。
+                            depthIndex++;
+                            //父节点设置为当前节点。
                             currentRoot = current;
                             index = 0;
                             return true;
@@ -267,8 +299,8 @@ namespace Megumin
                         return false;
                     }
 
-                    deepIndex--;
-                    if (deepIndex < 0)
+                    depthIndex--;
+                    if (depthIndex < 0)
                     {
                         return false;
                     }
@@ -291,7 +323,7 @@ namespace Megumin
             public void Reset()
             {
                 index = -1;
-                deepIndex = 0;
+                depthIndex = 0;
                 if (Interater.IncludeRoot)
                 {
                     currentRoot = null;
@@ -309,6 +341,35 @@ namespace Megumin
 
             }
         }
+
+#if UNITY_EDITOR
+
+        [UnityEditor.MenuItem("GameObject/Megumin/Transform DFS Depth3", priority = 40)]
+        static void DFSDepth3(UnityEditor.MenuCommand menuCommand)
+        {
+            if (menuCommand.context is GameObject go)
+            {
+                foreach (var item in new TransformDFS(go.transform))
+                {
+                    Debug.Log(item.name);
+                }
+            }
+        }
+
+        [UnityEditor.MenuItem("GameObject/Megumin/Transform DFS Depth All", priority = 41)]
+        static void DFSDepthAll(UnityEditor.MenuCommand menuCommand)
+        {
+            if (menuCommand.context is GameObject go)
+            {
+                foreach (var item in new TransformDFS(go.transform, 99))
+                {
+                    Debug.Log(item.name);
+                }
+            }
+        }
+
+#endif
+
     }
 }
 
